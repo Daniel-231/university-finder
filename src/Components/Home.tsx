626f1844b67189da2503336c0a6425607e115bd6
Daniel-231 <77553553+Daniel-231@users.noreply.github.com>
2022-08-06 15:36:17 +0300

    Update Home.tsx

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -4,14 +4,6 @@ import Navbar from "./Navbar";
 
 
 export const Home: React.FC = () => {
-  const [inputCountry, setInputCountry] = useState<any>();
-  const [data, setData] = useState([]);
-
-  function GetUniverities(e: any) {
-    const url = `http://universities.hipolabs.com/search?country=${inputCountry}`;
-    Axios.get(url).then((response) => setData(response.data));
-    e.preventDefault();
-  }
   
   return (
     <div>
